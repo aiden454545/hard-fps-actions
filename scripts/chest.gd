34e7fdabd3fec23698f8cdaa043d60586d34a07e
aiden454545 <minemann09@gmail.com>
2024-03-30 15:41:12 -0400

    fix actioninput node

--- scripts/chest.gd
+++ scripts/chest.gd
@@ -13,6 +13,7 @@ func open():
 	if is_open == false:
 		$AnimationPlayer.play("chest_open")
 		$Chest.queue_free()
+		$Chest2/CollisionShape3D.disabled = false
 		print("chest open")
 		is_interacting = false
 		needed_inputs.text = ""
@@ -21,21 +22,16 @@ func open():
 		current_inputs.visible = false
 		is_open = true
 
-
-func _input(event):
-	if event is InputEventKey and event.is_pressed() and is_interacting == true:
-		needed_inputs.text = "Needed Inputs: " + str(targetInputs)
-		keyboard_input.erase(0,1)
-		keyboard_input += event.as_text()
-		print(keyboard_input)
-		current_inputs.text = "Current Inputs: " + str(keyboard_input)
-		if keyboard_input == targetInputs and keyboard_input.length() <= stringLength:
-			open()
-			keyboard_input = ""
-		elif keyboard_input.length() > stringLength or keyboard_input.contains("R"):
-			keyboard_input = ""
+func _process(delta):
+	if Input.is_action_just_pressed("ui_end"):
+		is_interacting = false
 
 func _on_chest_interacted(body):
 	if is_open == false:
 		is_interacting = true
 
+
+
+func _on_input_sequence_inputs_complete():
+	if is_interacting == true:
+		open()
